71863d016dc55948285eb0f3f62f0ff79a216a61
Zafrir Ron <zafrir.ron@gmail.com>
2018-02-03 17:55:53 +0200

    Update ESP32-Bell-Lock.ino

--- ESP32-Bell-Lock.ino
+++ ESP32-Bell-Lock.ino
@@ -1,8 +1,5 @@
 /*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
  *
- *  You need to get streamId and privateKey at data.sparkfun.com and paste them
- *  below. Or just customize this script to talk to other HTTP servers.
  *
  */
 
